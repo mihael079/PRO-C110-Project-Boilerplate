349642d8935d79147cee7c3a34c65eb7666408b4
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-12-14 01:30:51 +0530

    Update PRO-C110-Project-Boilerplate.py

--- PRO-C110-Project-Boilerplate.py
+++ PRO-C110-Project-Boilerplate.py
@@ -1,33 +1,36 @@
-# for capturing frames from webcam
+# To Capture Frame
 import cv2
 
-# for processing images
+# To process image array
 import numpy as np
 
-# attaching webcam indexed as 0 with the application software
+# Attaching Cam indexed as 0, with the application software
 camera = cv2.VideoCapture(0)
 
 # Infinite loop
 while True:
 
-    # requesting a frame from camera
-    status , frame = camera.read()
+	# Reading / Requesting a Frame from the Camera 
+	status , frame = camera.read()
 
-    # if we were able to capture the frame successfully, status is 'true'
-    if status:
+	# if we were sucessfully able to read the frame
+	if status:
 
-        # flipping the frame
-        frame = cv2.flip(frame , 1)
+		# Flip the frame
+		frame = cv2.flip(frame , 1)
+		
+		# displaying the frames captured
+		cv2.imshow('feed' , frame)
 
-        # displaying the video feed
-        cv2.imshow('video feed' , frame)
+		# waiting for 1ms
+		code = cv2.waitKey(1)
+		
+		# if space key is pressed, break the loop
+		if code == 32:
+			break
 
-        # waiting for key press for 1ms
-        code = cv2.waitKey(1)
-
-        # if 'space' key is pressed, break
-        if code  ==  32:
-            break
-
+# release the camera from the application software
 camera.release()
+
+# close the open window
 cv2.destroyAllWindows()
